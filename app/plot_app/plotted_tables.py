b11a2a40b033255be698ac06a12453d4031ed301
unknown <dasig.jp2017.com>
2025-10-18 13:44:07 +0800

    PPTI DRONE FLIGHT REVIEW REPO

--- app/plot_app/plotted_tables.py
+++ app/plot_app/plotted_tables.py
@@ -62,7 +62,7 @@ def get_heading_html(ulog, px4_ulog, db_data, link_to_3d_page,
     if link_to_3d_page is not None and \
         any(elem.name == 'vehicle_gps_position' for elem in ulog.data_list):
         link_to_3d = ("<a class='btn btn-outline-primary' href='"+
-                      link_to_3d_page+"'>Open 3D View</a>")
+                      link_to_3d_page+"'>3D GAGO</a>")
     else:
         link_to_3d = ''
 
